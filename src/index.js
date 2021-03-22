8f9d7f7008932b6c15bdbe0e2819450ef950c04f
Carlj0666 <carlj1881@yahoo.com>
2021-03-21 21:56:19 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const userForm = document.getElementById("create-task-form");
+  userForm.addEventListener("submit", logSubmit);
+})
+
+
+function logSubmit(event) {
+  event.preventDefault(); //stop post
+  const newToDoDesc = document.getElementById("new-task-description");
+  const toDo = document.createElement("li");
+  const newToList = toDo.innerText = newToDoDesc.value;
+  const listElement = document.getElementById("tasks")
+  // listElement.innerText = newToList;
+  listElement.appendChild(toDo)
+}
